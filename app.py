373e44657bd6224104813639c787a56f70d70135
Llerenas <soporte@terravital.com.mx>
2025-07-08 14:08:27 -0700

    Update app.py
    cambios en https://railway.com/

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from controllers import controllers
 
@@ -5,4 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(controllers)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+       port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
